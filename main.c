8305d3fc4bc61a03b30a8af206167337171536ad
bulkset <chupapa327@rambler.ru>
2024-01-04 13:16:58 +0500

    Update main.c

--- main.c
+++ main.c
@@ -193,19 +193,19 @@ size_t my_atoi(const char *str) {
 }
 
 int my_strcmp(const char *str1, const char *str2) {
-    while (*str1 != '\0' && *str2 != '\0') {
-        if (*str1 != *str2) {
-            return (*str1 - *str2);
-        }
-        str1++;
-        str2++;
+  while (*str1 != '\0' && *str2 != '\0') {
+    if (*str1 != *str2) {
+      return (*str1 - *str2);
     }
+    str1++;
+    str2++;
+  }
 
-    if (*str1 == '\0' && *str2 == '\0') {
-        return 0;
-    }
+  if (*str1 == '\0' && *str2 == '\0') {
+    return 0;
+  }
 
-    return (*str1 - *str2);
+  return (*str1 - *str2);
 }
 
 char *my_strcpy(char *dest, const char *src) {
